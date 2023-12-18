101d19231b5e838bc8bf43cc8b69bf7b4bc69b8a
shengkai <maimemdada@gmail.com>
2023-12-18 13:54:33 +0700

    🔨 Frame UI

--- src/App.js
+++ src/App.js
@@ -5,9 +5,39 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Chào mừng bạn đến với Login Page</p>
-      <p>Đăng nhập để tiếp tục</p>
+      <div className="Box-Container">
+        <div className="Logo-container">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className="Text-welcome">Chào mừng bạn đến với Login Page</div>
+        <div>Đăng nhập để tiếp tục</div>
+
+        <div className="Form-container">
+          <label>Tài khoản</label>
+          <input></input>
+          <label>Mật khẩu</label>
+          <input></input>
+
+          <div className="Forget-password">
+            <div className="Container-check">
+              <div className="Check-Box" />
+              <label>Ghi nhớ mật khẩu</label>
+            </div>
+            <a>Quên mật khẩu</a>
+          </div>
+        </div>
+
+        <div>hoặc sử dụng</div>
+        {/* <div>hoặc sử dụng</div> */}
+
+        <button>Đăng nhập bằng tài khoản google</button>
+        <button>Đăng nhập</button>
+
+        <div>
+          <label>Bạn chưa có tài khoản?</label>
+          <a>Đăng ký tại đây</a>
+        </div>
+      </div>
     </div>
   );
 }
