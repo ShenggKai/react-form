4338be8394c0cc88cd544dae7068a78e96a7daae
shengkai <maimemdada@gmail.com>
2023-12-20 09:55:26 +0700

    💄 Add gradient border

--- src/App.js
+++ src/App.js
@@ -9,74 +9,71 @@ function App() {
 
   return (
     <div className="App">
-      <div className="Box-Container">
-        <div className="Header-container">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Space height={14} />
+      <div className="Gradient-border">
+        <div className="Box-Container">
+          <div className="Header-container">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Space height={14} />
+            <Text size={24.5}>Chào mừng bạn đến với Login Page</Text>
+            <Space height={14} />
+            <Text color={"#495057"}>Đăng nhập để tiếp tục</Text>
+            <Space height={28} />
+          </div>
 
-          <Text size={24.5}>Chào mừng bạn đến với Login Page</Text>
-          <Space height={14} />
-
-          <Text color={"#495057"}>Đăng nhập để tiếp tục</Text>
-          <Space height={28} />
-        </div>
-
-        <div className="Form-container">
-          <Input />
-          <Input isPassword={true} />
-
-          <div className="Forget-password">
-            <CheckBox
-              label={"Ghi nhớ mật khẩu"}
-              onClick={() => setChecked(!checked)}
-              checked={checked}
-            />
-            <Text
-              color={"#6366F1"}
-              cursor={"pointer"}
-              onClick={() => console.log("forgot")}
-            >
-              Quên mật khẩu
-            </Text>
+          <div className="Form-container">
+            <Input />
+            <Input isPassword={true} />
+            <div className="Forget-password">
+              <CheckBox
+                label={"Ghi nhớ mật khẩu"}
+                onClick={() => setChecked(!checked)}
+                checked={checked}
+              />
+              <Text
+                color={"#6366F1"}
+                cursor={"pointer"}
+                onClick={() => console.log("forgot")}
+              >
+                Quên mật khẩu
+              </Text>
+            </div>
+            <Space height={20} />
+            <Button haveIcon={false}>Đăng nhập</Button>
           </div>
           <Space height={20} />
 
-          <Button haveIcon={false}>Đăng nhập</Button>
-        </div>
-        <Space height={20} />
+          <div className="Divider">
+            <Line />
+            <Text color={"#495057"} fontWeight={400} width={"100%"}>
+              hoặc sử dụng
+            </Text>
+            <Line />
+          </div>
+          <Space height={20} />
 
-        <div className="Divider">
-          <Line />
-          <Text color={"#495057"} fontWeight={400} width={"100%"}>
-            hoặc sử dụng
-          </Text>
-          <Line />
-        </div>
-        <Space height={20} />
+          <Button haveIcon={true} Icon={<GoogleIcon />}>
+            Đăng nhập bằng tài khoản google
+          </Button>
+          <Space height={16} />
+          <Button haveIcon={true} Icon={<UserIcon />}>
+            Đăng nhập bằng tài khoản MobiFone
+          </Button>
+          <Space height={30} />
 
-        <Button haveIcon={true} Icon={<GoogleIcon />}>
-          Đăng nhập bằng tài khoản google
-        </Button>
-        <Space height={16} />
-
-        <Button haveIcon={true} Icon={<UserIcon />}>
-          Đăng nhập bằng tài khoản MobiFone
-        </Button>
-        <Space height={30} />
-
-        <div>
-          <Text color={"#495057"} fontWeight={400}>
-            {/* &nbsp; is the space*/}
-            Bạn chưa có tài khoản? &nbsp;
-          </Text>
-          <Text
-            color={"#6366F1"}
-            fontWeight={400}
-            cursor={"pointer"}
-            onClick={() => console.log("Sign")}
-          >
-            Đăng ký tại đây
-          </Text>
+          <div>
+            <Text color={"#495057"} fontWeight={400}>
+              {/* &nbsp; is the space*/}
+              Bạn chưa có tài khoản? &nbsp;
+            </Text>
+            <Text
+              color={"#6366F1"}
+              fontWeight={400}
+              cursor={"pointer"}
+              onClick={() => console.log("Sign")}
+            >
+              Đăng ký tại đây
+            </Text>
+          </div>
         </div>
       </div>
     </div>
