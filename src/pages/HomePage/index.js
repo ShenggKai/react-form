25f10c719f6f2cfc7b142f53eb9a3b936516ef84
shengkai <maimemdada@gmail.com>
2023-12-25 08:41:39 +0700

    🔨 Add router for app

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const HomePage = () => {
+  return (
+    <main>
+      <section>
+        <h2>Welcome to my homepage!</h2>
+        <p>This is a simple static home page built with React.</p>
+      </section>
+    </main>
+  );
+};
+
+export default HomePage;
