53e0b12a8b81d3fd6dabae276f4379ac0086a971
shengkai <maimemdada@gmail.com>
2023-12-27 10:57:10 +0700

    🍱 Add icon for menu

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -4,8 +4,11 @@ import { ReactComponent as Hide } from "./eye_hide.svg";
 import { ReactComponent as Show } from "./eye_show.svg";
 import { ReactComponent as Check } from "./check.svg";
 import { ReactComponent as Google } from "./google.svg";
-import { ReactComponent as User } from "./user.svg";
 import { ReactComponent as Close } from "./close.svg";
+import { ReactComponent as Home } from "./home.svg";
+import { ReactComponent as User } from "./user.svg";
+import { ReactComponent as About } from "./about.svg";
+import { ReactComponent as Blog } from "./blog.svg";
 
 export const HideIcon = ({ width = 20, height = 20 }) => {
   return <Hide width={width} height={height} />;
@@ -23,10 +26,22 @@ export const GoogleIcon = ({ width = 20, height = 20 }) => {
   return <Google width={width} height={height} />;
 };
 
-export const UserIcon = ({ width = 20, height = 20 }) => {
+export const CloseIcon = ({ width = 14, height = 14 }) => {
+  return <Close width={width} height={height} />;
+};
+
+export const HomeIcon = ({ width = 18, height = 18 }) => {
+  return <Home width={width} height={height} />;
+};
+
+export const UserIcon = ({ width = 18, height = 18 }) => {
   return <User width={width} height={height} />;
 };
 
-export const CloseIcon = ({ width = 14, height = 14 }) => {
-  return <Close width={width} height={height} />;
+export const AboutIcon = ({ width = 18, height = 18 }) => {
+  return <About width={width} height={height} />;
+};
+
+export const BlogIcon = ({ width = 18, height = 18 }) => {
+  return <Blog width={width} height={height} />;
 };
