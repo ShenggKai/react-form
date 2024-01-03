dab14e480a36722842dccab1a452cd1b1a70d266
shengkai <maimemdada@gmail.com>
2024-01-03 14:41:31 +0700

    🍱 Add new icons for checkbox, dropdown, paragraph, and radio

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -10,6 +10,10 @@ import { ReactComponent as User } from "./user.svg";
 import { ReactComponent as About } from "./about.svg";
 import { ReactComponent as Blog } from "./blog.svg";
 import { ReactComponent as Add } from "./add.svg";
+import { ReactComponent as Paragraph } from "./paragraph.svg";
+import { ReactComponent as Radio } from "./radio.svg";
+import { ReactComponent as Checkbox } from "./checkbox.svg";
+import { ReactComponent as Dropdown } from "./dropdown.svg";
 
 export const HideIcon = ({ width = 20, height = 20 }) => {
   return <Hide width={width} height={height} />;
@@ -49,4 +53,20 @@ export const BlogIcon = ({ width = 18, height = 18 }) => {
 
 export const AddIcon = ({ width = 35, height = 35 }) => {
   return <Add width={width} height={height} />;
+};
+
+export const ParagraphIcon = ({ width = 25, height = 25 }) => {
+  return <Paragraph width={width} height={height} />;
+};
+
+export const RadioIcon = ({ width = 25, height = 25 }) => {
+  return <Radio width={width} height={height} />;
+};
+
+export const CheckboxIcon = ({ width = 25, height = 25 }) => {
+  return <CheckboxIcon width={width} height={height} />;
+};
+
+export const DropdownIcon = ({ width = 25, height = 25 }) => {
+  return <Dropdown width={width} height={height} />;
 };
