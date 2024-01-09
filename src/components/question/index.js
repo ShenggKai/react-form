5ae45af310603c42a20d20aaa884e09ea7964671
shengkai <maimemdada@gmail.com>
2024-01-09 14:04:29 +0700

    🔨 Add image icon and input field for question title

--- src/components/question/index.js
+++ src/components/question/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Text, Dropdown, Line, Switch, OptionInput } from "../../components";
 import { useDispatch, useSelector } from "react-redux";
 import { changeTypeQuestion, removeQuestion } from "../../actions";
-import { BinIcon } from "../../assets";
+import { BinIcon, ImageIcon } from "../../assets";
 import "./style.css";
 
 const Question = () => {
@@ -24,7 +24,11 @@ const Question = () => {
         return (
           <div className="Question" key={field.questionID}>
             <div className="Question-header">
-              <Text size={18}>{field.title}</Text>
+              {/* <Text size={18}>{field.title}</Text> */}
+              <input placeholder="Question" defaultValue={field.title} className="Question-title" />
+              <div className="Add-image-icon">
+                <ImageIcon />
+              </div>
               <Dropdown
                 onChange={(selectedOption) => handleTypeChange(field.questionID, selectedOption)}
               />
