936c49dceead571f0b1fa7f8378d0714346ac593
shengkai <maimemdada@gmail.com>
2024-01-21 18:08:02 +0700

    🔨  Hover the option and drag icon appear

--- src/components/Form/OptionInput/index.js
+++ src/components/Form/OptionInput/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useState } from "react";
 import { CircleIcon, CloseIcon, RectangleIcon, DragIconVertical } from "../../../assets";
 import { Text, Line, Space } from "../../../components";
 import "./style.css";
@@ -24,6 +24,7 @@ const InactiveParagraph = () => {
 const AddOption = ({ type, options, handleAddOption, itemID }) => {
   return (
     <div className="Add-option">
+      <div className="Inactive-drag-vertical-icon" />
       {type === "multiple-choice" ? (
         <CircleIcon />
       ) : type === "checkbox" ? (
@@ -42,7 +43,18 @@ const AddOption = ({ type, options, handleAddOption, itemID }) => {
   );
 };
 
+const Icon = ({ type, index }) => {
+  return (
+    <>
+      {type === "multiple-choice" && <CircleIcon />}
+      {type === "checkbox" && <RectangleIcon />}
+      {type === "dropdown" && <div className="Dropdown-text-index">{index + 1}.</div>}
+    </>
+  );
+};
+
 const OptionInput = ({ field, isActive, addOption, removeOption, changeTextOption }) => {
+  const [indexHover, setIndexHover] = useState(null);
   const [type, options, itemID] = [field.type, field.options, field.itemID];
 
   const handleAddOption = (itemID) => {
@@ -57,59 +69,75 @@ const OptionInput = ({ field, isActive, addOption, removeOption, changeTextOptio
     changeTextOption(itemID, optionID, event.target.value);
   };
 
+  const handleHover = (optionID) => {
+    setIndexHover(optionID);
+  };
+
   return (
     <>
-      {type === "paragraph" ? (
-        isActive === itemID ? (
+      {isActive === itemID ? (
+        type === "paragraph" ? (
           <ActiveParagraph />
         ) : (
-          <InactiveParagraph />
-        )
-      ) : (
-        <div className="Multiple-input-container">
-          {options.map((option, index) => (
-            <div key={option.optionID} className="Multiple-input">
-              <DragIconVertical />
-              {type === "multiple-choice" ? (
-                <CircleIcon />
-              ) : type === "checkbox" ? (
-                <RectangleIcon />
-              ) : (
-                <div className="Dropdown-text-index">{index + 1}.</div>
-              )}
-              <input
-                type="text"
-                placeholder="your option"
-                value={option.content}
-                className={
-                  isActive === itemID ? "Multiple-input-text" : "Inactive-multiple-input-text"
-                }
-                autoFocus
-                onFocus={(event) => event.target.select()}
-                onChange={(event) => handleOptionTextChange(event, itemID, option.optionID)}
-              />
+          <div className="Multiple-input-container">
+            {options.map((option, index) => (
+              <div
+                key={option.optionID}
+                className="Multiple-input"
+                onMouseEnter={() => handleHover(option.optionID)}
+                onMouseLeave={() => handleHover(null)}
+              >
+                {indexHover === option.optionID ? (
+                  <div className="Drag-vertical-icon">
+                    <DragIconVertical />
+                  </div>
+                ) : (
+                  <div className="Inactive-drag-vertical-icon" />
+                )}
 
-              {options.length > 1 && isActive === itemID ? (
-                <div
-                  className="Remove-icon"
-                  title="Remove"
-                  onClick={() => handleRemoveOption(itemID, option.optionID)}
-                >
-                  <CloseIcon />
-                </div>
-              ) : (
-                <div className="Placeholder-icon" />
-              )}
-            </div>
-          ))}
-          {isActive === itemID && (
+                <Icon type={type} index={index} />
+
+                <input
+                  type="text"
+                  placeholder="your option"
+                  value={option.content}
+                  className="Multiple-input-text"
+                  autoFocus
+                  onFocus={(event) => event.target.select()}
+                  onChange={(event) => handleOptionTextChange(event, itemID, option.optionID)}
+                />
+
+                {options.length > 1 ? (
+                  <div
+                    className="Remove-icon"
+                    title="Remove"
+                    onClick={() => handleRemoveOption(itemID, option.optionID)}
+                  >
+                    <CloseIcon />
+                  </div>
+                ) : (
+                  <div className="Placeholder-icon" />
+                )}
+              </div>
+            ))}
             <AddOption
               type={type}
               options={options}
               handleAddOption={handleAddOption}
               itemID={itemID}
             />
-          )}
+          </div>
+        )
+      ) : type === "paragraph" ? (
+        <InactiveParagraph />
+      ) : (
+        <div className="Inactive-multiple-input-container">
+          {options.map((option, index) => (
+            <div key={option.optionID} className="Multiple-input">
+              <Icon type={type} index={index} />
+              <input value={option.content} className="Inactive-multiple-input-text" readOnly />
+            </div>
+          ))}
         </div>
       )}
     </>
